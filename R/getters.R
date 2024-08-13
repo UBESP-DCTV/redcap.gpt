3ad5d12984c252914bbfd176f7d07af63072f611
Corrado Lanera <corrado.lanera@gmail.com>
2024-08-13 21:48:06 +0200

    Add documentation to exported functions

--- R/getters.R
+++ R/getters.R
@@ -1,13 +1,34 @@
+#' Get the REDCap URI from the environment.
+#'
+#' @return (chr) The REDCap URI.
+#' @export
+#'
+#' @examples
+#' get_redcap_uri()
 get_redcap_uri <- function() {
   Sys.getenv("REDCAP_URI")
 }
 
+#' Get the REDCap project ID from the environment.
+#'
+#' @return (int) The REDCap project ID.
+#' @export
+#'
+#' @examples
+#' get_redcap_pid()
 get_redcap_pid <- function() {
-  Sys.getenv("REDCAP_PID") |> 
+  Sys.getenv("REDCAP_PID") |>
     as.integer()
 }
 
+#' Get the REDCap token from the environment.
+#'
+#' @return (chr) The REDCap token, invisibly.
+#' @export
+#'
+#' @examples
+#' get_redcap_token()
 get_redcap_token <- function() {
-  Sys.getenv("REDCAP_TOKEN") |> 
+  Sys.getenv("REDCAP_TOKEN") |>
     invisible()
 }

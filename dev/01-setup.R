bf01765f5323965b627ed9806003405e266e12e0
Corrado Lanera <corrado.lanera@gmail.com>
2024-08-13 11:36:18 +0200

    Setup dev environment
    Adds:
    README, NEWA, DESCRIPTION, LICENSE
    Supports:
    testthat, spellcheck

--- dev/01-setup.R
+++ dev/01-setup.R
@@ -1,11 +1,28 @@
 install.packages("pak")
 
-# dev
-pak::pkg_install(c(
-  # dev
-  "devtools", "tidyverse", "testthat", "checkmate",
-  # proj
-  "REDCapR"
-), dependencies = TRUE)
+dev_pkgs <- c(
+  "checkmate", "devtools", "magick", "testthat", "tidyverse"
+)
+prj_pkgs <- c("REDCapR")
+pak::pkg_install(c(dev_pkgs, prj_pkgs), dependencies = TRUE)
+
+use_description()
+use_mit_license()
+usethis::use_package_doc()
+usethis::use_readme_rmd()
+usethis::use_code_of_conduct("corrado.lanera@ubep.unipd.it")
+usethis::use_lifecycle_badge("experimental")
+usethis::use_logo("man/img/LAIMS.png")
+usethis::use_spell_check()
+usethis::use_testthat()
+usethis::git_vaccinate()
+
+dev_pkgs |> purrr::walk(\(x) usethis::use_package(x, type = "Suggests"))
+prj_pkgs |> purrr::walk(use_package)
 
 renv::snapshot()
+
+usethis::use_mit_license()
+usethis::use_tidy_description()
+
+usethis::use_news_md()

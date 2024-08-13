6f4c59f22967627ec7d42ff199d6a4f125e5de2d
Corrado Lanera <corrado.lanera@gmail.com>
2024-08-13 15:31:27 +0200

    add support to {REDCapTidieR}

--- dev/01-setup.R
+++ dev/01-setup.R
@@ -3,7 +3,7 @@ install.packages("pak")
 dev_pkgs <- c(
   "checkmate", "devtools", "magick", "testthat", "tidyverse"
 )
-prj_pkgs <- c("here", "REDCapR")
+prj_pkgs <- c("here", "REDCapR", "REDCapTidieR")
 pak::pkg_install(c(dev_pkgs, prj_pkgs), dependencies = TRUE)
 
 use_description()

341c4dc0bb89191985360dc96422efd5cb1fd05c
Corrado Lanera <corrado.lanera@gmail.com>
2024-08-15 02:18:47 +0200

    Update parsers for the answers provided by gpt

--- R/parse_answer.R
+++ R/parse_answer.R
@@ -1,52 +1,32 @@
-parse_answer <- function(
-  x,
-  what = c("sensazione", "momento", "andamento", "impatto")
-) {
-  checkmate::assert_string(x)
-  what <- match.arg(what)
-
-  parser <- switch (what,
-    sensazione = parse_sensazione,
-    momento = parse_momento,
-    andamento = parse_andamento,
-    impatto = parse_impatto
-  )
-
-  parser(x)
-}
-
 parse_sensazione <- function(x) {
-  checkmate::assert_string(x)
   stringi::stri_enc_toascii(stringr::str_to_lower(x)) == "si"
 }
 
-parse_momento <- function(x) {
-  checkmate::assert_string(x)
-  .levels <- c(
-    "Mattina (05-11)", "Pomeriggio (11-17)", "Sera (17-23)",
-    "Notte (23-05)"
-  )
+parse_gpt_fctr <- function(from_str, to_fct) {
+  .levels <- levels(to_fct)
 
   lover_level <- stringr::str_to_lower(.levels) |> 
     stringi::stri_enc_toascii() |> 
     stringr::str_remove_all("[^a-zA-Z]") |> 
     stringr::str_squish()
 
-  vec <- c("mattina", "pomeriggio", "Mattina", "Sera (17-23)")
+  res_str <- from_str |> 
+    dplyr::na_if("NA") |> 
+    stringr::str_to_lower() |>
+    stringi::stri_enc_toascii() |> 
+    stringr::str_remove_all("[^a-zA-Z]") |> 
+    stringr::str_squish() |> 
+    purrr::map_chr(\(x) {
+      res <- stringr::str_which(lover_level, stringr::fixed(x))
+      if (length(res) != 1) NA_character_ else .levels[res]
+    })
   
+  if (!all(res_str %in% .levels)) {
+    usethis::ui_warn("Not all stringrs parsed correctly.")
+    usethis::ui_info(
+      "Unique strings where {stringr::str_c(unique(res_str), collapse = ', ')}"
+    )
+  }
 
-
-
-
+  forcats::fct(res_str, .levels)
 }
-
-parse_andamento <- function(x) {
-  checkmate::assert_string(x)
-
-}
-
-parse_impatto <- function(x) {
-  checkmate::assert_string(x)
-
-}
-

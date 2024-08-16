0940554d6570fa0f6496722a0ec804cda890f781
Corrado Lanera <corrado.lanera@gmail.com>
2024-08-16 16:51:56 +0200

    Update write example

--- analyses/write.R
+++ analyses/write.R
@@ -1,12 +1,12 @@
 library(redcap.gpt)
 library(REDCapR)
 
-db <- fetch_redcap()
+# db <- fetch_redcap()
 fup_143060 <- fetch_form("followup_postoperatorio_14_30_60_giorno_po")
 # fup_90 <- fetch_form("visita_followup_postoperatorio_90_giorno_po")
 
 note_fup_to_be_pushed <- fup_143060 |> 
-  query_gpt_on_redcap_instrument("note_fup")
+    query_gpt_on_redcap_instrument("note_fup")
 
 # - no missing values on logical
 REDCapR::validate_for_write(note_fup_to_be_pushed)

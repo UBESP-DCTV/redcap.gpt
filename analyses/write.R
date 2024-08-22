1f934826ed9a63ca0faa0096b3f77d597c65eb31
Corrado Lanera <corrado.lanera@gmail.com>
2024-08-22 02:55:24 +0200

    update example write to process all records

--- analyses/write.R
+++ analyses/write.R
@@ -6,7 +6,7 @@ fup_143060 <- fetch_form("followup_postoperatorio_14_30_60_giorno_po")
 fup_90 <- fetch_form("visita_followup_postoperatorio_90_giorno_po")
 
 note_fup_to_be_pushed <- fup_143060 |> 
-    query_gpt_on_redcap_instrument("note_fup")
+  query_gpt_on_redcap_instrument("note_fup", query_on_all_records = TRUE)
 comments_fup_to_be_pushed <- fup_143060 |> 
   query_gpt_on_redcap_instrument("comments_fup")
 details_fup_to_be_pushed <- fup_90 |> 
